363897e9ce1b6608eb811aadbf8c1e3d607e2f1b
JingpengZhang <jingpeng_zhang@foxmail.com>
2023-12-21 17:02:48 +0800

    feat:完善 /user/create 接口的错误提示

--- src/modules/user/joi/user.password.joi.ts
+++ src/modules/user/joi/user.password.joi.ts
@@ -1,5 +1,13 @@
 import * as Joi from 'joi';
 
 export const userPasswordJoi = Joi.object({
-  password: Joi.string().regex(/^(?![^a-zA-Z]+$)(?!\D+$)/).error(new Error('密码格式不正确,长度最少8位,且至少包含一个字母和一个数字')),
-})
+  password: Joi.string()
+    .required()
+    .regex(/^(?![^a-zA-Z]+$)(?!\D+$)/)
+    .messages({
+      'any.required': '密码必填',
+      'string.empty': '密码不能为空',
+      'string.pattern.base':
+        '密码格式不正确,长度最少8位,且至少包含一个字母和一个数字',
+    }),
+});
